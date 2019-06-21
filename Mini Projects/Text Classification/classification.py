9b0451ca028edb7be9202c5b5d47ee7c9e0c8cb1
Prathamesh99 <prathamesh.borhade29@gmail.com>
2019-06-21 20:20:56 +0530

    final commit

--- Mini Projects/Text Classification/classification.py
+++ Mini Projects/Text Classification/classification.py
@@ -25,8 +25,28 @@ for i in range(len(X)):
 	review = re.sub(r'\s+',' ',review)
 	corpus.append(review)
 	
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-vectorizer = CountVectorizer(max_features=2000, min_df=3, max_df=0.6, stop_words=stopwords.words('english'))
-transformer = TfidfTransformer()
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(max_features=2000, min_df=3, max_df=0.6, stop_words=stopwords.words('english'))
 X = vectorizer.fit_transform(corpus).toarray()
-X = transformer.fit_transform(X).toarray()
+
+from sklearn.model_selection import train_test_split
+text_train, text_test, sent_train, sent_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(text_train, sent_train)
+sent_pred = classifier.predict(text_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(sent_test, sent_pred)
+accuracy = (cm[0][0]+cm[1][1])/4
+print(accuracy)
+
+with open("classifier.pickle","wb") as f:
+	pickle.dump(classifier,f)
+	
+with open("tfidfmodel.pickle","wb") as f:
+	pickle.dump(vectorizer,f)
+	
